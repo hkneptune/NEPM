b33aca5856dc0e562fb2263aefe8487311cd028e
Neptune LI <hkneptune@gmail.com>
2019-07-26 14:56:04 +0800

    Added Users model

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize')
 
 const ENV = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.json'))[ENV];
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const dbConn = new Sequelize(config.database, config.username, config.password, config);
 const db = {};
 
 Object.keys(db).forEach((modelName) => {
@@ -21,12 +21,12 @@ fs.readdirSync(__dirname).filter((file) => {
 
 }).forEach((file) => {
 
-  let model = sequelize.import(path.join(__dirname, file))
+  let model = dbConn.import(path.join(__dirname, file))
   db[model.name] = model
 
 })
 
-db.sequelize = sequelize
+db.sequelize = dbConn
 db.Sequelize = Sequelize
 
 module.exports = db
---
+++ src/models/users.js
@@ -0,0 +1,68 @@
+module.exports = (sequelize, Sequelize) => {
+
+  let Users = sequelize.define('sg_users', {
+
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    uuid: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      notEmpty: true,
+      unique: true
+    },
+    first_name: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      validate: {
+        len: [1, 128],
+        min: 1,
+        max: 128
+      }
+    },
+    last_name: {
+      type: Sequelize.STRING,
+      notEmpty: true,
+      validate: {
+        len: [1, 128]
+      }
+    },
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      notEmpty: true,
+      unique: true,
+      validate: {
+        len: [6, 32]
+      }
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        validate: {
+          len: [6, 64]
+        }
+      }
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      notEmpty: true,
+    },
+    last_login: {
+      type: Sequelize.DATE(6)
+    },
+    active: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true
+    }
+  })
+
+  return Users
+}
