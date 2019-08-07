cf2f07aa6c5b92acff4e036eea000657eada9130
Neptune LI <hkneptune@gmail.com>
2019-08-07 16:00:29 +0800

    Updated the links, checked if the user has already logged in, added cookie max age

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json())
 app.use(session({
   secret: 'This is a secret used in session',
   resave: true,
+  cookie: { maxAge: 30000 }, // 30 seconds
   saveUninitialized: true })); // Session secret
 app.use(passport.initialize());
 app.use(passport.session()); // Persistent login sessions
@@ -42,27 +43,41 @@ function isLoggedIn(req, res, next) {
   res.redirect('/user/login?login=0')
 }
 
+function isLoggedOut(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard')
+  }
+
+  return next()
+}
+
+function showHomeButton(isMember) {
+  return isMember ? '<a href="/">Home</a><br /><a href="/dashboard">Dashboard</a><br />' : '<a href="/">Home</a><br />'
+}
+
+function showAuthButtons(isMember) {
+  return isMember ? '<a href="/user/logout">Logout</a><br />' : '<a href="/user/register">Register</a><br /><a href="/user/login">Login</a><br />'
+}
+
+function showUserName(user) {
+  return user ? 'Hello, <strong>' + user.first_name + ' ' + user.last_name + '</strong>!<br />' : 'Hello, <strong>Anonymous</strong>!<br />'
+}
+
 // Testing path
 app.get('/', (req, res) => {
-  let greeting = 'Hello World!'
-  let member = '<a href="/user/login">Login</a>'
-  if (req.user) {
-    greeting = 'Hello ' + req.user.first_name + ' ' + req.user.last_name
-    member = '<a href="/user/logout">Logout</a>'
-  }
-  res.send('<a href="/">Home</a><br /><a href="/user/register">Register</a><br />' + member + '<br />' + greeting + '!')
+  return res.send(showHomeButton(req.user) + showAuthButtons(req.user) + showUserName(req.user))
 })
 
-app.get('/user/register', (req, res) => {
-  res.send('<a href="/">Home</a><br /><a href="/user/login">Login</a><br /><form id="signup" name="signup" method="post" action="/api/auth/signup"><label for="email">Email Address</label>&nbsp;<input class="text" name="email" type="email" /><br /><label for="first_name">First Name</label>&nbsp;<input name="first_name" type="text" /><br /><label for="last_name">Last Name</label>&nbsp;<input name="last_name" type="text" /><br /><label for="password">Password</label>&nbsp;<input name="password" type="password" /><br /><input class="btn" type="submit" value="Sign Up" /></form>')
+app.get('/user/register', isLoggedOut, (req, res) => {
+  res.send(showHomeButton(req.user) + showAuthButtons(req.user) + showUserName(req.user) + '<br /><form id="signup" name="signup" method="post" action="/api/auth/signup"><label for="email">Email Address</label>&nbsp;<input class="text" name="email" type="email" /><br /><label for="first_name">First Name</label>&nbsp;<input name="first_name" type="text" /><br /><label for="last_name">Last Name</label>&nbsp;<input name="last_name" type="text" /><br /><label for="password">Password</label>&nbsp;<input name="password" type="password" /><br /><input class="btn" type="submit" value="Sign Up" /></form>')
 })
 
-app.get('/user/login', (req, res) => {
-  res.send('<a href="/">Home</a><br /><a href="/user/register">Register</a><br /><form id="signin" name="signin" method="post" action="/api/auth/signin"><label for="email">Email Address</label>&nbsp;<input class="text" name="email" type="text" /><br /><label for="password">Password</label>&nbsp;<input name="password" type="password" /><br /><input class="btn" type="submit" value="Sign In" /></form>')
+app.get('/user/login', isLoggedOut, (req, res) => {
+  res.send(showHomeButton(req.user) + showAuthButtons(req.user) + showUserName(req.user) + '<br /><form id="signin" name="signin" method="post" action="/api/auth/signin"><label for="email">Email Address</label>&nbsp;<input class="text" name="email" type="text" /><br /><label for="password">Password</label>&nbsp;<input name="password" type="password" /><br /><input class="btn" type="submit" value="Sign In" /></form>')
 })
 
 app.get('/dashboard', isLoggedIn, (req, res) => {
-  res.send('<a href="/user/logout">Logout</a>')
+  res.send(showHomeButton(req.user) + showAuthButtons(req.user) + showUserName(req.user))
 })
 
 app.post('/api/auth/signup', passport.authenticate('local-signup', {
@@ -71,7 +86,7 @@ app.post('/api/auth/signup', passport.authenticate('local-signup', {
 }))
 
 app.post('/api/auth/signin', passport.authenticate('local-signin', {
-  successRedirect: '/?login=2',
+  successRedirect: '/dashboard',
   failureRedirect: '/user/login?fail=1'
 }))
 
